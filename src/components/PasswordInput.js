c8e365880e7c18e5d10202831a8d8efe7c2145c5
florianGierlichs <gierlichs@gmx.de>
2020-03-31 20:58:23 +0200

    Add different color to password input

--- src/components/PasswordInput.js
+++ src/components/PasswordInput.js
@@ -1,5 +1,34 @@
 import React from "react";
+import styled from "@emotion/styled";
 
-export default function PasswordInput({ value }) {
-  return <input value={value} />;
+const Input = styled.input`
+  background-color: hsl(
+    ${props => (props.value?.length <= 10 ? props.value?.length * 10 : 100)},
+    100%,
+    50%
+  );
+`;
+
+const Inputt = styled.input`
+  background-color: hsl(
+    ${props => (if(props.value?.length === 0) {
+
+    })},
+    100%,
+    50%
+  );
+`;
+
+export default function PasswordInput() {
+  const [inputValue, setInputValue] = React.useState(null);
+
+  return (
+    <Input
+      value={inputValue}
+      type="password"
+      onChange={event => {
+        setInputValue(event.target.value);
+      }}
+    />
+  );
 }
